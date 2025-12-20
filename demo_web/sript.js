1a99f56e4324f0200d2909f83e8bbbcae5fc9fb5
Nxtwave-web <fuadkabbo76@gmail.com>
2025-12-20 18:44:07 +0600

    Delete demo_web directory

--- demo_web/sript.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
-
-  if (window.scrollY > 50) {
-    navbar.style.background = "#111";
-  } else {
-    navbar.style.background = "transparent";
-  }
-});
